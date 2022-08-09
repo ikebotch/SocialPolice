7520ef5bde1b162f5196243a87988f84681508ab
Patrick Adutwum <adutwum.pa@gmail.com>
2022-08-09 13:20:41 +0000

    connect splash to ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:socialpolice/src/ui/bottm_nav.dart';
+import 'package:socialpolice/src/ui/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BottomNav(),
+      home: Splash(),
     );
   }
 }
--- lib/src/ui/splash.dart
+++ lib/src/ui/splash.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:socialpolice/src/res/colors.dart';
+import 'package:socialpolice/src/ui/bottm_nav.dart';
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 600), () {
+      setState(() {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const BottomNav(),
+          ),
+        );
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
