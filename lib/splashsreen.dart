e0123b370c296c8d17486c4b3556fb040e472a28
Gerald-Aggrey <833hagan@gmail.com>
2022-09-17 08:13:44 +0000

    splashcreen and custom montserrat fonsts added

---
+++ lib/splashsreen.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:socialpolice/login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => LoginScreen())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Center(
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Color(0xff276EF1),
+                    Color(0xffFFFFFF),
+                  ],
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  'SLEECS',
+                  style: TextStyle(
+                    shadows: [
+                      Shadow(
+                          color: Colors.black.withOpacity(0.3),
+                          offset: const Offset(1, 5),
+                          blurRadius: 15),
+                    ],
+                    fontFamily: 'Montserrat-Bold',
+                    fontSize: 48.0,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
