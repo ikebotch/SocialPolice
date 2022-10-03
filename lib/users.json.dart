276936c0b2de8076cb4fe46c795ca3e04d595d13
Nene Kumoji <kevlucks@gmail.com>
2022-10-03 13:35:09 +0000

    json

---
+++ lib/users.json.dart
@@ -0,0 +1,22 @@
+[ {"count": 2,
+"next": null,
+"previous": null,
+"results",
+{
+"incident"
+"citizen":
+"officer" : 1,
+"status"
+1,
+"media" :
+"http://127.0.0.1:8000/media/reports/DALLE_2022-08-19_22.16.12_-a_dog_in_a_lab_coat_doing_chemistry._digital_art.png"
+"location"
+"Tema"}
+"incident": 2,
+"citizen":
+“officer" : 1,
+"status'
+1
+"media" :
+"http://127.0.0.1:8000/media/reports/DALLE_2022-08-19_21.40.30_-brain_space_surreal.png"}
+"location": "accra"} ]
